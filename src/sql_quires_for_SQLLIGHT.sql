c30c14a0ec14f1848b6096885407f4e1160480ff
Gayantha Chamith <73245735+gayanthachamith@users.noreply.github.com>
2026-02-06 12:25:23 +0200

    sql practice

---
+++ src/sql_quires_for_SQLLIGHT.sql
@@ -0,0 +1,34 @@
+
+
+CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    completed BOOLEAN
+);
+
+
+INSERT INTO tasks (title, description, completed)
+VALUES
+('Learn SQL', 'Practice basic SQL commands', 0),
+('Finish assignment', 'Complete SQLite practice task', 1),
+('Review Java', 'Revise exception handling', 0);
+
+
+SELECT * FROM tasks;
+
+
+SELECT * FROM tasks
+WHERE completed = 1;
+
+
+UPDATE tasks
+SET completed = 1
+WHERE id = 1;
+
+
+DELETE FROM tasks
+WHERE id = 3;
+
+DROP TABLE tasks;
+
